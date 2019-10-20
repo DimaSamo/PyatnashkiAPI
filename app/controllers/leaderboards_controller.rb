47aa7daded626a555a21f5ec9c18e46a8ef3a770
DimaSamo <dimasamo96@gmail.com>
2019-10-20 05:41:20 -0400

    remove requirement of authenticity token for form

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,13 +1,16 @@
+require 'pry'
 class LeaderboardsController < ApplicationController
-
+    skip_before_action :verify_authenticity_token
     def index
+        # binding.pry
         leaderboards = Leaderboard.all.sorted
         render json: leaderboards
     end
 
     def create
+        #binding.pry
         lb = Leaderboard.new(leaderboard_params)
-        if @lb.save
+        if lb.save
             render json: {status:200, message: "Leaderboard entry added."}
         else
             render json: {status: 500, message: "Failed to add entry."}
